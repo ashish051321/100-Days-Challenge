f914f1080890c994d584f0e323b56f5783dd5fb1
ashish mishra <ashish.begin@gmail.com>
2018-02-14 15:07:56 +0530

    done

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
     let x =10;//just some dummy assignment
     let y =20;// some dummy assignment
+    let z = 3 + 5;
     let myProjectContainer = document.querySelector("div.container.myprojects");
     let myprojects = myProjectContainer.querySelectorAll("div.col-lg-4");
     // alert(myprojects.length);
